8c70fc2f91b00fff03c5407f81074c1ab02773e3
krishv2002 <93152515+krishv2002@users.noreply.github.com>
2023-02-16 09:11:19 +0530

    Create new_file.cpp

---
+++ main/new_file.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  //cout << "Hello, World PES1UG20CS219\n";
+  //cout << "Hello, Jenkins PES1UG20CS219\n";
+  cout << "I have successfully built and run 2nd cpp file.\nI'am PES1UG20CS219";
+  return 0;
+}
